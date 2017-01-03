9334748a16fd74872e55fa07c29ca57cab521e0e
Kenny Batista <kennybatista7@gmail.com>
2017-01-03 04:51:38 -0500

    done for the day

--- TripCard/TripCard/TripViewController.swift
+++ TripCard/TripCard/TripViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TripViewController: UIViewController {
+class TripViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     // Collection View
     @IBOutlet var collectionView: UICollectionView!
@@ -27,6 +27,89 @@ class TripViewController: UIViewController {
         backgroundImageView.addSubview(blurEffectView)
         
     }
+    
+    
+    
+    
+    private var trips = [Trip(tripId: "Paris001", city: "Paris", country: "France",
+                              featuredImage: UIImage(named: "paris"), price: 2000, totalDays: 5, isLiked:
+        false),
+                         Trip(tripId: "Rome001", city: "Rome", country: "Italy", featuredImage:
+                            UIImage(named: "rome"), price: 800, totalDays: 3, isLiked: false),
+                         Trip(tripId: "Istanbul001", city: "Istanbul", country: "Turkey",
+                              featuredImage: UIImage(named: "istanbul"), price: 2200, totalDays: 10, isLiked:
+                            false),
+                         Trip(tripId: "London001", city: "London", country: "United Kingdom",
+                              featuredImage: UIImage(named: "london"), price: 3000, totalDays: 4, isLiked:
+                            false),
+                         Trip(tripId: "Sydney001", city: "Sydney", country: "Australia",
+                              featuredImage: UIImage(named: "sydney"), price: 2500, totalDays: 8, isLiked:
+                            false),
+                         Trip(tripId: "Santorini001", city: "Santorini", country: "Greece",
+                              featuredImage: UIImage(named: "santorini"), price: 1800, totalDays: 7, isLiked:
+                            false),
+                         Trip(tripId: "NewYork001", city: "New York", country: "United States",
+                              featuredImage: UIImage(named: "newyork"), price: 900, totalDays: 3, isLiked:
+                            false),
+                         Trip(tripId: "Kyoto001", city: "Kyoto", country: "Japan", featuredImage:
+                            UIImage(named: "kyoto"), price: 1000, totalDays: 5, isLiked: false)
+    ]
+    
+    
+    
+//    func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return 1
+//    }
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection
+//        section: Int) -> Int {
+//        return trips.count
+//    }
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt
+//        indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
+//                                                      for: indexPath) as! TripCollectionViewCell
+//        // Configure the cell
+//        cell.cityLabel.text = trips[indexPath.row].city
+//        cell.countryLabel.text = trips[indexPath.row].country
+//        cell.imageView.image = trips[indexPath.row].featuredImage
+//        cell.priceLabel.text = "$\(String(trips[indexPath.row].price))"
+//        cell.totalDaysLabel.text = "\(trips[indexPath.row].totalDays) days"
+//        cell.isLiked = trips[indexPath.row].isLiked
+//        
+//        
+//        // apply round corner
+//        cell.layer.cornerRadius = 4.0
+//        return cell
+//    }
+    
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return trips.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        // create the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! TripCollectionViewCell
+        
+        // customize the cell
+        cell.cityLabel.text = trips[indexPath.row].city
+        cell.countryLabel.text = trips[indexPath.row].country
+        cell.imageView.image = trips[indexPath.row].featuredImage
+        cell.daysLabel.text = String(trips[indexPath.row].totalDays)
+        cell.priceLabel.text = "$\(trips[indexPath.row].price)"
+        cell.isLiked = trips[indexPath.row].isLiked
+        
+        return cell
+    }
+    
+    
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
